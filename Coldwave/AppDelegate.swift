2718e1c44e41dfaf62277f14d138c08f47b1c891
Andrew Byrd <andrew@fastmail.net>
2015-01-02 11:58:28 +0100

    Initial Commit

---
+++ Coldwave/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  Coldwave
+//
+//  Created by Andrew Byrd on 1/2/15.
+//  Copyright (c) 2015 Andrew Byrd. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    /* Initialize application */
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+
+    }
+
+    /* Tear down application */
+    func applicationWillTerminate(_ aNotification: Notification) {
+
+    }
+
+}
+
---
+++ Podfile
@@ -0,0 +1,15 @@
+# Uncomment the next line to define a global platform for your project
+# platform :macos, '10.15'
+
+target 'Coldwave' do
+  use_frameworks! 
+
+  # Pods for Coldwave
+  pod 'OrigamiEngine/Flac', :git => 'https://github.com/andyshep/OrigamiEngine.git', :commit => '2b35e0e8262cfe3b623f6508ef45c9c97bf65383', :submodules => true
+
+  target 'ColdwaveTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
