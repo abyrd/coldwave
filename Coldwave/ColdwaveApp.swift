f25829503ea6552bb4758bdb298c9c96555179fc
Andrew Byrd <andrew@fastmail.net>
2021-07-05 19:51:52 +0800

    Initial Commit

---
+++ Coldwave/ColdwaveApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColdwaveApp.swift
+//  Coldwave
+//
+//  Created by Andrew Byrd on 05/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct ColdwaveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
