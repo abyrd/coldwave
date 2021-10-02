5893e8e16d117c344d7986bb7e4bfa44c7dd680f
Andrew Byrd <abyrd@conveyal.com>
2021-10-02 23:48:19 +0800

    Use AVPlayer instead of AVAudioPlayer
    Player automatically moves to next track.
    Slider shows play progress, cover enlarges to full window on play.
    Artist and title styling, removed dead code.
    
    AVPlayer seems slightly but not clearly better than AVAudioPlayer.

--- Coldwave/ColdwaveApp.swift
+++ Coldwave/ColdwaveApp.swift
@@ -8,9 +8,6 @@
 import SwiftUI
 import AVFoundation
 
-var timer: Timer?
-var player: AVAudioPlayer?
-
 @main
 struct ColdwaveApp: App {
     
@@ -18,9 +15,7 @@ struct ColdwaveApp: App {
     
     var body: some Scene {
         WindowGroup {
-            ContentView(state: state).onAppear() {
-                // Setup here
-            }
+            ContentView(state: state)
         }
         .commands {
             CommandGroup(before: CommandGroupPlacement.newItem) {
@@ -75,14 +70,59 @@ struct ColdwaveApp: App {
     }
 }
 
-// TODO add notes on using ObservableObject and Published instead of State variables.
-
 class ColdwaveState: ObservableObject {
     @Published var albums: [Album] = []
     @Published var path: String = "";
     @Published var currentTrack = 0
     @Published var coverSize: CGFloat = DEFAULT_IMAGE_SIZE
     @Published var playlist: [URL]  = []
+    @Published var amountPlayed: Double = 0.0 // in range 0...1
     @Published var playing: Bool = false
+
+    let player: AVPlayer = AVPlayer()
+
+    init() {
+        player.addPeriodicTimeObserver(
+            forInterval: CMTime(seconds: 0.5, preferredTimescale: CMTimeScale(NSEC_PER_SEC)),
+            queue: .main
+        ) { t in
+            // Compare two CMTimeScales and update slider via state.
+            // Assuming duration and current time are in same units for now
+            if let duration = self.player.currentItem?.duration.convertScale(t.timescale, method: CMTimeRoundingMethod.default) {
+                self.amountPlayed = Double(t.value) / Double(duration.value)
+            }
+        }
+    }
+    
+    // It doesn't seem clean to put this (or the AVPlayer) on the state, but notification
+    // targets have to be objc functions which have to be members of an NSObject or protocol.
+    // I could probably factor the player field and this method out into another class.
+    @objc func playerDidFinishPlaying(sender: Notification) {
+        print("End of track \(currentTrack), advancing.")
+        jumpToTrack(currentTrack + 1)
+    }
+    
+    // TODO implement auto-next-track (with crossfade using setVolume(t) ?)
+    func jumpToTrack (_ trackNumber: Int) {
+        if (trackNumber >= 0 && trackNumber < playlist.count) {
+            let track = AVPlayerItem(asset: AVAsset(url: playlist[trackNumber]))
+            player.replaceCurrentItem(with: track)
+            player.play()
+            currentTrack = trackNumber;
+            playing = true
+            // Deregister any previously registered end-of-track notifications
+            NotificationCenter.default.removeObserver(self)
+            NotificationCenter.default.addObserver(self,
+                selector: #selector(playerDidFinishPlaying(sender:)),
+                name: NSNotification.Name.AVPlayerItemDidPlayToEndTime,
+                object: track
+            )
+        } else {
+            player.pause()
+            playing = false
+        }
+    }
+
+
 }
 
--- Coldwave/ContentView.swift
+++ Coldwave/ContentView.swift
@@ -16,143 +16,133 @@ let DEFAULT_IMAGE_SIZE: CGFloat = 400
 struct ContentView: View {
     
     @ObservedObject var state: ColdwaveState
+    @State var currentAlbum: Album? = nil
+    @State var searchText: String = ""
     
-    @State var amountPlayed: Double = 0.0 // in range 0...1
-    @State var playing: Bool = false
-
     private let padding: CGFloat = 20
-    
+        
     var body: some View {
         VStack {
-            ScrollView {
-                LazyVGrid(
-                    columns: [GridItem(.adaptive(minimum: state.coverSize))],
-                    spacing: padding
-                ){
-                    // ForEach with an initial capital F is a SwiftUI view, not the language construct.
-                    // We should somehow hint to the renderer that the result of the ForEach view can only change
-                    // when a new directory is opened. The approach of regenerating the view just to change the album
-                    // size seems really inefficient, but maybe it is able to efficiently detect the situation.
-                    // Maybe the ObservedObject should only contain the top level state, and all derived state should
-                    // be in @state variables or normal variables.
-                    ForEach(state.albums, id: \.self) { album in
-                        VStack {
-                            Image(nsImage: NSImage(contentsOfFile: album.coverImagePath) ??
-                                    NSImage(named: "record-sleeve-\(abs(album.title.hashValue % 2)).png")!)
-                                .resizable()
-                                .frame(width: state.coverSize, height: state.coverSize, alignment: .center)
-                                //.border(Color.black)
-                                .shadow(radius: 4)
-                            // Ideally name would be vertically oriented beside album cover, like spine text, but
-                            // using HStack with Text().rotationEffect(Angle(degrees: -90)) causes misalignment
-                            Text(album.artist)
-                            Text(album.title)
-                        }
-                        .onTapGesture {
-                            state.playlist = album.getPlaylist() as [URL]
-                            jumpToTrack(0);
-                        }
-                    }
-                }.padding(padding)
+            // Text search box. This intentionally draws the keyboard focus away from the track selector
+            // or time slider. They look less than great with a permanent fat blue focus ring.
+            HStack {
+                Image(systemName: "magnifyingglass")
+                TextField("Search", text: $searchText,
+                    onCommit: {
+                        print("onCommit")
+                    }).foregroundColor(.primary)
+                Image(systemName: "xmark.circle.fill").opacity(searchText == "" ? 0 : 1).onTapGesture {
+                    searchText = ""
+                }
             }
-            
-            //.onAppear() {
-            //}
-            
-            // Look up systemImage icons (which are just a font) with SF Symbols https://developer.apple.com/sf-symbols/
-            // There might already be color inverted versions of these symbols.
-            
-            // We could also completely hide these controls in the menu and accept only hotkeys
+            .padding(EdgeInsets(top: 8, leading: 6, bottom: 8, trailing: 6))
+            .foregroundColor(.secondary)
+            .cornerRadius(10.0)
+
+            // Either a full-size album cover or a scrollable view of all album covers if no album is playing.
+            if (state.playing) {
+                Image(nsImage: currentAlbum!.coverAsNSImage())
+                    .resizable()
+                    //.frame(width: state.coverSize, height: state.coverSize, alignment: .center)
+                    .border(Color.black)
+                    .aspectRatio(contentMode: .fit)
+                    .padding(padding)
+            } else {
+                ScrollView {
+                    LazyVGrid(
+                        columns: [GridItem(.adaptive(minimum: state.coverSize))],
+                        spacing: padding
+                    ){
+                        // ForEach with an initial capital F is a SwiftUI view, not the language construct.
+                        // We should somehow hint to the renderer that the result of the ForEach view can only change
+                        // when a new directory is opened. The approach of regenerating the view just to change the album
+                        // size seems really inefficient, but maybe it is able to efficiently detect the situation.
+                        // Maybe the ObservedObject should only contain the top level state, and all derived state should
+                        // be in @state variables or normal variables.
+                        ForEach(state.albums, id: \.self) { album in
+                            VStack {
+                                Image(nsImage: album.coverAsNSImage())
+                                    .resizable()
+                                    .frame(width: state.coverSize, height: state.coverSize, alignment: .center)
+                                    .border(Color.black)
+                                    .shadow(radius: 4)
+                                // Ideally name would be vertically oriented beside album cover, like spine text, but
+                                // using HStack with Text().rotationEffect(Angle(degrees: -90)) causes misalignment
+                                Text(album.artist).bold()
+                                Text(album.title).italic()
+                            }
+                            .onTapGesture(count: 2) {
+                                currentAlbum = album;
+                                state.playlist = album.getPlaylist()
+                                state.jumpToTrack(0)
+                            }
+                        }
+                    }.padding(padding)
+                }
+            }
+            // Horizonal row of controls at the bottom of the window.
+            // We could also completely hide these controls in the menu and accept only hotkeys.
+            // Use Image views with tap gesture listeners - they are simpler with less chrome than buttons.
             HStack() {
                 Image(systemName: "backward.end.fill").padding(5).border(Color.black).onTapGesture {
                     print("PREV")
-                    if let p = player, p.isPlaying, p.currentTime < 2 {
-                        jumpToTrack(state.currentTrack)
-                    } else {
-                        jumpToTrack(state.currentTrack - 1);
+                    state.jumpToTrack(state.currentTrack - 1)
+                }
+                Image(systemName: "play.fill").padding(5).border(Color.black, width: state.playing ? 3 : 1).onTapGesture {
+                    print("PLAY")
+                    if (state.player.currentItem != nil) {
+                        state.player.play()
+                        state.playing = true
                     }
                 }
-                // Comment on Button("Label", action: {}) {Image()} as opposed to Image().onTapGesture()
-                // Button has a lot more chrome around it, moves when clicked, and looks more cluttered.
-                Button(action: {
-                        print("PLAY")
-                        if let p = player {
-                            if !p.isPlaying {
-                                p.play()
-                                state.playing = true;
-                            }
-                        } else {
-                            jumpToTrack(state.currentTrack);
-                        }
-                }) { Image(systemName: "play.fill") }
-                .padding(5)
-                .background(playing ? Color.gray : Color.white)
-                .buttonStyle(BorderlessButtonStyle())
-                
-                Image(systemName: "pause.fill").padding(5).border(Color.black).onTapGesture {
+                Image(systemName: "pause.fill").padding(5).border(Color.black, width: state.playing ? 1 : 3).onTapGesture {
                     print("PAUSE")
-                    player?.pause()
+                    state.player.pause()
                     state.playing = false
                 }
                 Image(systemName: "forward.end.fill").padding(5).border(Color.black).onTapGesture {
                     print("NEXT")
-                    jumpToTrack(state.currentTrack + 1);
+                    state.jumpToTrack(state.currentTrack + 1);
                 }
-                let selectedItem = (state.playlist.isEmpty) ? "Album Tracks" : state.playlist[state.currentTrack].lastPathComponent
+                let selectedItem = (state.playlist.isEmpty)
+                    ? "Album Tracks"
+                    : state.playlist[state.currentTrack].lastPathComponent
                 Menu(selectedItem) {
                     ForEach(state.playlist.indices, id: \.self) { trackIndex in
                         Button(state.playlist[trackIndex].lastPathComponent) {
-                            jumpToTrack(trackIndex)
+                            state.jumpToTrack(trackIndex)
                         }.id(trackIndex)
                     }
-                }
-            }.padding(5)
-            Slider(
-                value: Binding(
-                    get: {
-                        let duration = player?.duration ?? 0;
-                        if (duration == 0) {
-                            return 0;
-                        } else {
-                            return (player?.currentTime ?? 0) / duration
-                        }
-                    },
-                    set: {
-                        newValue in player?.play(atTime: player!.duration * newValue)
+                }.focusable(false)
+            }.padding(padding)
+            // When slider is moved, trailing closure is called with true, then false when released.
+            // Dragging is quite unresponsive, maybe because the UI is recomputed when state changes.
+            Slider(value: $state.amountPlayed, in: 0...1) {editing in
+                if (!editing) {
+                    if let d = state.player.currentItem?.duration {
+                        let newPosition = (Double(d.value) * state.amountPlayed)
+                        state.player.seek(to: CMTimeMake(value: Int64(newPosition), timescale: d.timescale))
                     }
-                ),
-                in: 0...1
-            )
-        }
-    }
-
-    // TODO implement auto-next-track (with crossfade using setVolume(t) ?)
-    
-    // /System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Versions/A/Support/lsregister -dump | grep flac
-    // Shows UTI org.xiph.flac
-    
-    private func jumpToTrack (_ trackNumber: Int) {
-        if (trackNumber >= 0 && trackNumber < state.playlist.count) {
-            // Cleanly stop any existing player. It will be garbage collected when we dereference it.
-            player?.stop()
-            do {
-                player = try AVAudioPlayer(contentsOf: state.playlist[trackNumber], fileTypeHint: "org.xiph.flac")
-                guard let player = player else { return }
-                player.play()
-                state.currentTrack = trackNumber;
-                playing = true
-            } catch let error {
-                print(error.localizedDescription)
+                }
             }
+            // Text(String(state.amountPlayed))
         }
+
     }
     
+    private func mmss (seconds: Int) -> String {
+        let minutes = seconds / 60
+        let seconds = seconds - minutes * 60
+        return String(format: "%3i:%02i", minutes, seconds)
+    }
+    
+
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         Group {
-            ContentView(state: ColdwaveState())
+            ContentView(state: ColdwaveState(), currentAlbum: nil)
         }
     }
 }
